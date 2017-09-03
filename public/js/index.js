f44074d35a8096e12bc8af114d3342b89e555da1
georgedimmitrov <georgedimmitrov@gmail.com>
2017-09-03 17:43:47 +0300

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server.');
-
-  socket.emit('createMessage', {
-    from: 'joro',
-    text: 'sample text'
-  });
 });
 
 socket.on('disconnect', function() {
