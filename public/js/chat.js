09e1b3ca9f298948ecfe7ac245a5a3cee574c0f2
georgedimmitrov <georgedimmitrov@gmail.com>
2017-09-04 07:39:27 +0300

    send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -74,7 +74,6 @@ $('#message-form').on('submit', function(ev) {
   var messageTextbox = $('[name="message"]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function() {
     messageTextbox.val('');
